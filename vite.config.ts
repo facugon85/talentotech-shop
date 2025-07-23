44ebb8313d60f1f7fd8946256cede33af9ea1474
cufa <facundonic.gonzalez@gmail.com>
2025-07-23 19:09:42 -0300

    Deploy inicial para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/talento_tech_entrega/', // 👈 importante
   plugins: [react()],
+  base: '/talentotech-shop/', 
 })
