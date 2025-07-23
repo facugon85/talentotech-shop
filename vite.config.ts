3536684bbc27f08143f3af2c7a8abf297cfc7989
cufa <facundonic.gonzalez@gmail.com>
2025-07-23 20:21:17 -0300

    Proyecto React inicializado con Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { BrowserRouter as Router } from 'react-router-dom';
 
 export default defineConfig({
   plugins: [react()],
   base: '/talentotech-shop/',
 })
-
-<Router basename="/talentotech-shop">
